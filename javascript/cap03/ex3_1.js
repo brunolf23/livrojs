3be40e0ccb34c5877850bf14bab9c559ccc2eaab
Bruno Lopes <bruno.lf@hotmail.com>
2025-01-27 16:21:39 -0300

    adicionando node exe1 e 2

---
+++ javascript/cap03/ex3_1.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")() // adiciona pacote para entrada de dados
+const num1 = Number(prompt("1º Número: ")) // lê os números
+const num2 = Number(prompt("2ª Número: "))
+const soma = num1 + num2 // a soma dos números
+console.log(`A soma é: ${soma}`) // exibir o resultado
