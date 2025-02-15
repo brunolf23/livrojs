76451d56e2094fbf51d6a12237e026b9f396ba18
Bruno Lopes <bruno.lf@hotmail.com>
2025-02-15 20:33:17 -0300

    cap5 exe 13

---
+++ javascript/cap05/js/ex5_13.js
@@ -0,0 +1,28 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const num = Number(frm.inNumero.value)
+
+    let divisores = "Divisores do "+ num + ": 1"
+    let soma = 1
+
+    for (let i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            divisores = divisores + ", " + i
+            soma = soma + i
+        }
+    }
+    divisores = divisores + " (Soma: " + soma + ")"
+
+    resp.innerText = divisores
+
+    if (num == soma) {
+        resp2.innerText = `${num} É um Número Perfeito`
+    } else {
+        resp2.innerText = `${num} Não É um Número Perfeito`
+    }
+})
