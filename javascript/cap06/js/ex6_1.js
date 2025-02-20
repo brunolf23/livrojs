1bb6b382e98eb84b764afbfb48a74a69bd4d5552
Bruno Lopes <bruno.lf@hotmail.com>
2025-02-20 20:37:48 -0300

    cap6 exe 1 v5

--- javascript/cap06/js/ex6_1.js
+++ javascript/cap06/js/ex6_1.js
@@ -18,3 +18,32 @@ frm.addEventListener("submit", (e) => {
     frm.inPaciente.value = ""
     frm.inPaciente.focus()
 })
+
+frm.btUrgencia.addEventListener("click", () => {
+    if (!frm.checkValidity()) {
+        alert(`Informe o nome do paciente a ser atendido em caráter de urgência`)
+        frm.inPaciente.focus()
+        return
+    }
+    const nome = frm.inPaciente.value
+    pacientes.unshift(nome)
+    let lista = ""
+
+    pacientes.forEach((pacientes, i) => (lista += `${i + 1}. ${pacientes}\n`))
+    respLista.innerText = lista
+    frm.inPaciente.value = ""
+    frm.inPaciente.focus()
+})
+
+frm.btAtender.addEventListener("click", () => {
+    if (pacientes.length == 0) {
+        alert(`Não há pacientes na lista de espera`)
+        frm.pacientes.focus()
+        return
+    }
+    const atender = pacientes.shift()
+    respNome.innerText = atender
+    let lista = ""
+    pacientes.forEach((paciente, i) => (lista += `${i + 1}. ${paciente}\n`))
+    respLista.innerText = lista
+})
