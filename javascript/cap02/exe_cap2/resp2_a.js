be0d99a903f59cb5fcac79bbddfa4018164a983e
Bruno Lopes <bruno.lf@hotmail.com>
2025-04-08 18:18:38 -0300

    cap 2 exe a v2

--- javascript/cap02/exe_cap2/resp2_a.js
+++ javascript/cap02/exe_cap2/resp2_a.js
@@ -2,6 +2,7 @@
 const frm = document.querySelector("form");
 const texto1 = document.querySelector("#resp1");
 const texto2 = document.querySelector("#resp2");
+const texto3 = document.querySelector("#resp3");
 
 // ação para o botão submit for clicado
 frm.addEventListener("submit", (e) => {
@@ -11,9 +12,14 @@ frm.addEventListener("submit", (e) => {
     // math.flor calcula o valor arredondando para baixo
     const soma = Math.floor(valorDoProduto * 2)
 
+    // math.raound calucula os centavos que sobraram
+    const valorTotal = valorDoProduto * 2
+    const economia = Math.round((valorTotal % 1) * 100)
+
     // exibe a resposta
     texto1.innerText = `Promoção de ${produto}`
     texto2.innerText = `Leve 2 por apenas R$: ${soma.toFixed(2)}`
+    texto3.innerText = `Desconto de R$: ${economia} centavos`
 
     e.preventDefault()
 });
